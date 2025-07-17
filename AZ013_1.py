368c55529dd14a7ef8d00c5ba8070612fb46d432
Sergio2490 <merido2490@gmail.com>
2025-07-18 00:21:37 +0300

    Update AZ013_1.py

--- AZ013_1.py
+++ AZ013_1.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('World-happiness-report-2024.csv')
-print(df.info())
+#print(df.info())  #инфо
+#print(df.describe())  № статистич данные - мин, макс, среднее
+#print(df['Country name']) #выводим столбец
+#print(df[['Country name', 'Regional indicator']])  #выводим несколько столбцов
+print(df.loc[56]) #выводим строчку по индексу
