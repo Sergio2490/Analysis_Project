06872ee306900fa72d7d267be92414d40a8da557
Sergio2490 <merido2490@gmail.com>
2025-07-20 00:15:43 +0300

    All_AZ013_1_1video

--- AZ013_1.py
+++ AZ013_1.py
@@ -5,4 +5,7 @@ df = pd.read_csv('World-happiness-report-2024.csv')
 #print(df.describe())  № статистич данные - мин, макс, среднее
 #print(df['Country name']) #выводим столбец
 #print(df[['Country name', 'Regional indicator']])  #выводим несколько столбцов
-print(df.loc[56]) #выводим строчку по индексу
+#print(df.loc[56]) #выводим строчку по индексу
+#print(df.loc[56, 'Healthy life expectancy'])
+print(df[df['Healthy life expectancy'] > 0.7])
+
