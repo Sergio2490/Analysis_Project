2fbeb3e3d95545e1729e36417a5606c517946f7a
Sergio2490 <merido2490@gmail.com>
2025-07-21 23:48:11 +0300

    Update AZ01_3.py

--- AZ01_3.py
+++ AZ01_3.py
@@ -1,2 +1,11 @@
 import pandas as pd
 
+df = pd.read_csv('hh.csv', delimiter = ';')  #Сначала читаем csv-файл
+
+#df['Test'] = [new for new in range(50)]
+
+#print(df)
+
+#df.drop('Test', axis = 1, inplace = True)  #Удаляем столбец Test
+df.drop(49, axis = 0, inplace = True)
+print(df)
