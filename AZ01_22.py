8e169b8e44ecc125c82adb2dbc3425a7f4f0a7ce
Sergio2490 <merido2490@gmail.com>
2025-07-31 00:29:41 +0300

    Create AZ01_22.py

---
+++ AZ01_22.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Создадим простой словарик, чт сделать из него датафрейм
+data = {'value': [1,2,2,3,3,3,4,4,4,5,6,7,8,9,10,55]}
+
+df = pd.DataFrame(data)  #Сделали из словаря датафрейм
+
+df['value'].hist() #Рисуем гистограмму
+plt.show() #Показываем гистограмму
