74c4e804b667c56c1179eb415951059892655c92
Sergio2490 <merido2490@gmail.com>
2025-08-27 23:36:02 +0300

    Update AZ02_31.py

--- AZ02_31.py
+++ AZ02_31.py
@@ -1,1 +1,17 @@
 #Категориальные данные
+
+import pandas as pd
+
+data = {
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'gender': ['female', 'male', 'male', 'male', 'female'],
+    'department': ['HR', 'Engeneering', 'Marketing', 'Engeneering', 'HR']
+}
+
+#Сделаем из этого словаря датафрейм
+df = pd.DataFrame(data)
+
+#Создадим категории - по полу и по департаменту
+df['gender'] = df['gender'].astype('category')
+df['department'] = df['department'].astype('category')
+
